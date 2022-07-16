28df45e355f33787ac809ccda64656ca61bce9d1
Gerald Wong <email@geraldwong.net>
2022-07-17 00:49:51 +0800

    updated how game modes work

--- code/game_mode_splash.h
+++ code/game_mode_splash.h
@@ -5,5 +5,48 @@
 
 #include "game.h"
 
+//////////////////////////////////////////////////
+// SPLASH MODE
+//
+struct Splash {
+  F32 timer;
+};
 
-#endif //GAME_MODE_SPLASH_H
+static void
+splash_init(Game* game) 
+{
+  auto* splash = game_allocate_mode<Splash>(game);
+  splash->timer = 1.f;
+}
+
+static void 
+splash_tick(Game* game,
+            Game_Input* input,
+            Painter* painter) 
+{
+  auto* splash = (Splash*)game->mode_context;
+  
+  F32 dt = input->seconds_since_last_frame;
+  splash->timer -= dt;
+  
+  if (splash->timer < 0.f) {
+    //game_set_mode(game, 0, 0); 
+
+    game_set_mode(game, splash_init, splash_tick);
+  }
+ 
+  auto color = rgba(splash->timer, splash->timer, splash->timer, splash->timer);
+  paint_text(painter,
+             FONT_DEFAULT, 
+             string_from_lit("momo"),
+             color,
+             450.f, 400.f, 
+             256.f);
+  advance_depth(painter);
+}
+
+
+
+#endif // GAME_MODE_SPLASH_H
+
+
