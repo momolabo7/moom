eece53813df537e40f7a49ce173665ab8da0faa7
Gerald Wong <email@geraldwong.net>
2023-11-10 22:51:52 +0900

    updated aoc

--- code/aoc22.cpp
+++ code/aoc22.cpp
@@ -388,5 +388,200 @@ aoc22_d4b(const char* filename) {
 
 }
 
+static void 
+aoc22_d5a(const char* filename) 
+{
+  FILE* fp = fopen(filename, "r");
+  if (fp) {
+    char buffer[128];
+    u32_t stack_count = 0; 
+    u32_t original_highest = 0;
+
+    // One pass to initialize data structure
+    while(fgets(buffer, sizeof(buffer), fp)) {
+      if ( buffer[0] == '\n' || buffer[0] == '\r' ) break;
+
+      static u32_t first = 0;
+      if (!first) {
+        stack_count = cstr_count_readables(buffer)/3;  
+        first = 1;
+      }
+      ++original_highest;    
+    }
+    fseek(fp, 0, SEEK_SET);
+    original_highest -= 1;
+
+    u32_t max_height = stack_count*original_highest;
+
+    //printf("%d %d\n", stack_count, original_highest);
+
+    char* aoc22_d5_stacks = (char*)calloc(stack_count * max_height + (stack_count * sizeof(u32_t)), 1);
+    u32_t* stack_heights = (u32_t*)(aoc22_d5_stacks + stack_count * max_height);
+
+#define aoc22_d5_stacks_at(i,j) (*(stacks+(i*max_height)+j))
+
+    if (aoc22_d5_stacks) 
+    {
+      //for (s32_t h = original_highest-1; h >= 0; --h) {
+      for (u32_t h = 0; h < original_highest; ++h) {
+        fgets(buffer, sizeof(buffer), fp);
+        for (u32_t i = 1, stack_index = 0; stack_index < stack_count ; i += 4, ++stack_index) {
+          if (is_uppercase(buffer[i])) {
+            //printf("oi: %c @ %d %d\n", buffer[i], stack_index, h);
+            aoc22_d5_stacks_at(stack_index, original_highest - h - 1) = buffer[i];
+            stack_heights[stack_index]++;
+
+          }
+        }
+
+      }
+
+#define aoc22_d5_stacks_print() \
+      for (u32_t i = 0; i < stack_count ; ++i){ \
+        printf("#%d: ", i+1); \
+        for (u32_t j = 0; j < stack_heights[i]; ++j) { \
+          printf("%c ", aoc22_d5_stacks_at(i,j)); \
+        }\
+        printf(" = %d\n", stack_heights[i]);\
+      } 
+
+      //aoc22_d5_stacks_print();
+
+      fgets(buffer, sizeof(buffer), fp);
+      fgets(buffer, sizeof(buffer), fp);
+
+#define aoc22_d5_stacks_pop(i) stacks_at(i, --stack_heights[i])
+#define aoc22_d5_stacks_push(i, item) stacks_at(i, stack_heights[i]++) = (item)
+#define aoc22_d5_stacks_last(i) stacks_at(i, stack_heights[i]-1)
+
+      while(fgets(buffer, sizeof(buffer), fp)) {
+        tokenizer_t t = { buffer, 0 }; 
+        u32_t move_amount = tokenizer_get_next_u32(&t);
+        u32_t from_index = tokenizer_get_next_u32(&t) - 1;
+        u32_t to_index = tokenizer_get_next_u32(&t) - 1;
+        //printf("move %d from #%d to #%d\n", move_amount, from_index+1, to_index+1);
+        for(u32_t move_index = 0; move_index < move_amount; ++move_index)
+        {
+          // pop from 'from'
+          char popped = aoc22_d5_stacks_pop(from_index);
+          aoc22_d5_stacks_push(to_index, popped);
+        }
+        //aoc22_d5_stacks_print();
+      }
+
+      //printf("answer: ");
+      for(u32_t stack_index = 0; stack_index < stack_count; ++stack_index){
+        printf("%c", aoc22_d5_stacks_last(stack_index));
+      }
+      //printf("\n");
+      free(aoc22_d5_stacks);
+    }
+
+    fclose(fp);
+  }
+
+
+}
+
+static void 
+aoc22_d5b(const char* filename) 
+{
+  FILE* fp = fopen(filename, "r");
+  if (fp) {
+    char buffer[128];
+    u32_t stack_count = 0; 
+    u32_t original_highest = 0;
+
+    // One pass to initialize data structure
+    while(fgets(buffer, sizeof(buffer), fp)) {
+      if ( buffer[0] == '\n' || buffer[0] == '\r' ) break;
+
+      static u32_t first = 0;
+      if (!first) {
+        stack_count = cstr_count_readables(buffer)/3;  
+        first = 1;
+      }
+      ++original_highest;    
+    }
+    fseek(fp, 0, SEEK_SET);
+    original_highest -= 1;
+
+    u32_t max_height = stack_count*original_highest;
+
+    //printf("%d %d\n", stack_count, original_highest);
+
+    char* aoc22_d5_stacks = (char*)calloc(stack_count * max_height + (stack_count * sizeof(u32_t)), 1);
+    u32_t* stack_heights = (u32_t*)(aoc22_d5_stacks + stack_count * max_height);
+
+#define aoc22_d5_stacks_at(i,j) (*(stacks+(i*max_height)+j))
+
+    if (aoc22_d5_stacks) 
+    {
+      //for (u32_t h = original_highest-1; h >= 0; --h) {
+      for (u32_t h = 0; h < original_highest; ++h) {
+        fgets(buffer, sizeof(buffer), fp);
+        for (u32_t i = 1, stack_index = 0; stack_index < stack_count ; i += 4, ++stack_index) {
+          if (is_uppercase(buffer[i])) {
+            //printf("oi: %c @ %d %d\n", buffer[i], stack_index, h);
+            aoc22_d5_stacks_at(stack_index, original_highest - h - 1) = buffer[i];
+            stack_heights[stack_index]++;
+
+          }
+        }
+
+      }
+
+#define aoc22_d5_stacks_print() \
+      for (u32_t i = 0; i < stack_count ; ++i){ \
+        printf("#%d: ", i+1); \
+        for (u32_t j = 0; j < stack_heights[i]; ++j) { \
+          printf("%c ", aoc22_d5_stacks_at(i,j)); \
+        }\
+        printf(" = %d\n", stack_heights[i]);\
+      } 
+
+      //aoc22_d5_stacks_print();
+
+      fgets(buffer, sizeof(buffer), fp);
+      fgets(buffer, sizeof(buffer), fp);
+
+#define aoc22_d5_stacks_last(i) stacks_at(i, stack_heights[i]-1)
+
+
+      while(fgets(buffer, sizeof(buffer), fp)) {
+        tokenizer_t t = { buffer, 0 }; 
+        u32_t move_amount = tokenizer_get_next_u32(&t);
+        u32_t from_index = tokenizer_get_next_u32(&t) - 1;
+        u32_t to_index = tokenizer_get_next_u32(&t) - 1;
+        //printf("move %d from #%d to #%d\n", move_amount, from_index+1, to_index+1);
+
+        u32_t from_beg = stack_heights[from_index]-move_amount; 
+        u32_t from_ope = stack_heights[from_index];
+
+        u32_t to_beg = stack_heights[to_index]; 
+
+        for(;from_beg < from_ope; ++from_beg){
+          aoc22_d5_stacks_at(to_index, to_beg++) = stacks_at(from_index, from_beg);
+        }
+        stack_heights[from_index] -= move_amount;
+        stack_heights[to_index] += move_amount;
+
+        //aoc22_d5_stacks_print();
+      }
+
+      //printf("answer: ");
+      for(u32_t stack_index = 0; stack_index < stack_count; ++stack_index){
+        printf("%c", aoc22_d5_stacks_last(stack_index));
+      }
+      //printf("\n");
+
+      free(aoc22_d5_stacks);
+    }
+
+    fclose(fp);
+  }
+
+
+}
 int main() {
 }
