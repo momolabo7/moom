d10ceed521e6ebc060d457efa352ae56d897fba3
Gerald Wong <email@geraldwong.net>
2024-03-18 20:35:01 +0900

    updated

--- code/test_fileio_api.cpp
+++ code/test_fileio_api.cpp
@@ -85,6 +85,24 @@ os_file_read(os_file_t* fp, u8_t* dest, usz_t size, usz_t offset) {
   }
 }
 
+static b32_t
+os_file_write(os_file_t* fp, const u8_t* src, usz_t size, usz_t offset) 
+{
+  OVERLAPPED overledened = {};
+  overledened.Offset = (u32_t)((offset >> 0) & 0xFFFFFFFF);
+  overledened.OffsetHigh = (u32_t)((offset >> 32) & 0xFFFFFFFF);
+  
+  DWORD bytes_wrote;
+  if(WriteFile(fp->handle, src, (DWORD)size, &bytes_wrote, &overledened) &&
+     (DWORD)size == bytes_wrote) 
+  {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
 static u64_t
 os_file_size(os_file_t* fp) {
   LARGE_INTEGER file_size;
@@ -104,8 +122,7 @@ static void
 os_file_close(os_file_t* fp) 
 {
   if (fp->handle) {
-    // TODO
-    //close(fp->handle);
+    close(fp->handle);
   }
 }
 
@@ -149,6 +166,18 @@ os_file_read(os_file_t* fp, u8_t* dest, usz_t size, usz_t offset) {
   return true;
 }
 
+static b32_t
+os_file_write(os_file_t* fp, const u8_t* src, usz_t size, usz_t offset) 
+{
+  if (lseek(fp->handle, offset, SEEK_SET) == -1) {
+    return false;
+  }
+  if (write(fp->handle, src, size) == -1) {
+    return false;
+  }
+  return true;
+}
+
 static u64_t
 os_file_size(os_file_t* fp) {
   // NOTE(momo): We don't have to 'reset' the seek position 
@@ -185,14 +214,32 @@ os_read_file_into_str(const char* filename, arena_t* arena, b32_t null_terminate
   return ret;
 }
 
+static b32_t
+os_write_str_to_file(const char* filename, str_t buffer) {
+  os_file_t file = {};
+  if (!os_file_open(&file, filename, OS_FILE_ACCESS_CREATE)) {
+    return false; 
+  }
+
+  defer { os_file_close(&file); };
+  os_file_write(&file, buffer.e, buffer.size, 0);
+
+  return true;
+}
 
 int main() 
 {
   arena_t arena = {};
   arena_alloc(&arena, gigabytes(1)); 
+  defer { arena_free(&arena); }; 
+
   str_t str = os_read_file_into_str("test.csv", &arena);
   if (str) {
     printf("%s",str.e);
   }
-  printf("done\n");
+  
+  if (!os_write_str_to_file("test3.csv", str)) {
+    return str_bad();
+  }
+
 }
